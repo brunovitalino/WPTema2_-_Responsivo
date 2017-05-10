2737648394d3193c7245eac42cf401c66ec08044
Bruno <Bruno@BrunoNB.localdomain>
2017-05-10 16:21:00 -0300

    Implementado carousel.
    Signed-off-by: Bruno <Bruno@BrunoNB.localdomain>

--- bvresponsivo/header.php
+++ bvresponsivo/header.php
@@ -37,13 +37,3 @@
 		  </div><!-- /.container -->
 		</nav>
 	</div>
-
-	<div class="slider">
-		<img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/assets/images/slider-01.jpg">
-	</div>
-
-	<div class="container">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in rhoncus tellus. Phasellus tempor a odio et tincidunt. Phasellus vitae nibh blandit nibh congue aliquam nec nec mauris. Pellentesque fermentum a est ac egestas. Phasellus ipsum eros, molestie sit amet placerat sed, efficitur at sem. Aliquam porta lobortis lorem quis ullamcorper. Sed dui nibh, ornare a turpis quis, interdum posuere nunc. Nam in risus ut diam tristique tincidunt. Pellentesque volutpat tristique sagittis. Maecenas egestas molestie scelerisque. Donec vestibulum vestibulum neque eget placerat. Nunc eu sapien elit. Cras lacinia ex a aliquam eleifend. Sed id eros vitae lacus porttitor gravida. Vivamus mollis, turpis ac convallis pharetra, libero dui ultricies turpis, vehicula mollis turpis nibh elementum ante.</p> 
-		<p>Morbi dapibus purus vitae blandit viverra. Nulla vulputate mi id dolor faucibus aliquam. Sed lobortis nisi metus, quis facilisis nunc euismod in. Quisque a gravida justo. Donec non consectetur orci, vel eleifend orci. Proin sit amet velit nec leo placerat ullamcorper sed at purus. Vivamus consequat tempor sagittis. Maecenas scelerisque, lectus non placerat faucibus, ligula risus fermentum sapien, nec euismod nunc nisl eget quam. Fusce fermentum lacinia eros et eleifend. Sed felis quam, finibus maximus urna in, tristique iaculis neque. Etiam pellentesque est ante, at tincidunt nisl maximus vel. Aliquam erat volutpat. Curabitur tortor arcu, tincidunt nec vestibulum at, euismod sit amet enim. Suspendisse potenti. Vestibulum tempor a tortor a ultricies. Praesent id tempor augue.</p> 
-		<p>In feugiat odio massa, sed vestibulum dolor dignissim sed. Nam non auctor nibh. Maecenas quis ligula nunc. Duis commodo feugiat orci, ut ultrices ante tempor in. Quisque ut elit tempus, luctus orci at, aliquam purus. Maecenas cursus, quam in ultricies vestibulum, magna eros efficitur magna, eget aliquam mauris purus nec elit. Nulla blandit magna nec tortor tristique facilisis.</p>
-	</div>
--- bvresponsivo/index.php
+++ bvresponsivo/index.php
@@ -1,4 +1,50 @@
 <?php get_header() ?>
 
+	<div class="slider">
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		      <img src="<?php bloginfo('template_directory'); ?>/assets/images/slider-01.jpg" alt="...">
+		      <div class="carousel-caption">
+		        <h2>Estiloso...</h2>
+		        <a class="button-leia-mais" href="#">LEIA MAIS</a>
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="<?php bloginfo('template_directory'); ?>/assets/images/slider-02.jpg" alt="...">
+		      <div class="carousel-caption">
+		        <h2>Ágil...</h2>
+		        <a class="button-leia-mais" href="#">LEIA MAIS</a>
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="<?php bloginfo('template_directory'); ?>/assets/images/slider-03.jpg" alt="...">
+		      <div class="carousel-caption">
+		        <h2>Elétrico...</h2>
+		        <a class="button-leia-mais" href="#">LEIA MAIS</a>
+		      </div>
+		    </div>
+		    
+		  </div>
+
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+	</div>
 
 <?php get_footer() ?>
