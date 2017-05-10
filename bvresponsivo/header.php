8d60f0c8631559fef3a97f96ee90fa84e6017cbe
Bruno <Bruno@BrunoNB.localdomain>
2017-05-10 11:09:14 -0300

    Criada estrutura inicial do nosso tema.
    Signed-off-by: Bruno <Bruno@BrunoNB.localdomain>

---
+++ bvresponsivo/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php wp_title(); ?></title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<div class="container">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in rhoncus tellus. Phasellus tempor a odio et tincidunt. Phasellus vitae nibh blandit nibh congue aliquam nec nec mauris. Pellentesque fermentum a est ac egestas. Phasellus ipsum eros, molestie sit amet placerat sed, efficitur at sem. Aliquam porta lobortis lorem quis ullamcorper. Sed dui nibh, ornare a turpis quis, interdum posuere nunc. Nam in risus ut diam tristique tincidunt. Pellentesque volutpat tristique sagittis. Maecenas egestas molestie scelerisque. Donec vestibulum vestibulum neque eget placerat. Nunc eu sapien elit. Cras lacinia ex a aliquam eleifend. Sed id eros vitae lacus porttitor gravida. Vivamus mollis, turpis ac convallis pharetra, libero dui ultricies turpis, vehicula mollis turpis nibh elementum ante.</p> 
+		<p>Morbi dapibus purus vitae blandit viverra. Nulla vulputate mi id dolor faucibus aliquam. Sed lobortis nisi metus, quis facilisis nunc euismod in. Quisque a gravida justo. Donec non consectetur orci, vel eleifend orci. Proin sit amet velit nec leo placerat ullamcorper sed at purus. Vivamus consequat tempor sagittis. Maecenas scelerisque, lectus non placerat faucibus, ligula risus fermentum sapien, nec euismod nunc nisl eget quam. Fusce fermentum lacinia eros et eleifend. Sed felis quam, finibus maximus urna in, tristique iaculis neque. Etiam pellentesque est ante, at tincidunt nisl maximus vel. Aliquam erat volutpat. Curabitur tortor arcu, tincidunt nec vestibulum at, euismod sit amet enim. Suspendisse potenti. Vestibulum tempor a tortor a ultricies. Praesent id tempor augue.</p> 
+		<p>In feugiat odio massa, sed vestibulum dolor dignissim sed. Nam non auctor nibh. Maecenas quis ligula nunc. Duis commodo feugiat orci, ut ultrices ante tempor in. Quisque ut elit tempus, luctus orci at, aliquam purus. Maecenas cursus, quam in ultricies vestibulum, magna eros efficitur magna, eget aliquam mauris purus nec elit. Nulla blandit magna nec tortor tristique facilisis.</p> 
+
+	</div>
